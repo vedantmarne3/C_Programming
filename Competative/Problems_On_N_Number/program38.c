ae898b2e96359ec36466aa3e6ef9b99aeca31720
vedantmarne3 <vedantbmarne@gmail.com>
2026-07-05 23:51:47 +0530

    Added new programs

---
+++ Competative/Problems_On_N_Number/program38.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int EvenFactorial(int iNo)
+{
+    int iCnt = 0;
+    int iFact = 1;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        if(iCnt % 2 == 0)
+        {
+            iFact = iFact * iCnt;
+        }
+    }
+
+    return iFact;
+    
+}
+
+int main()
+{
+    int iValue = 0, iRet = 0;
+
+    printf("Enter Number : \n");
+    scanf("%d",&iValue);
+
+    iRet = EvenFactorial(iValue);
+
+    printf("Even Factorial of Number is : %d",iRet);
+
+    return 0;
+}
