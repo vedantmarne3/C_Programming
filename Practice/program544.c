0b070b717daa03532e74086e759d212975e11b8c
vedantmarne3 <vedantbmarne@gmail.com>
2026-07-22 23:57:25 +0530

    Added new programs

---
+++ Practice/program544.c
@@ -0,0 +1,16 @@
+//Recurtion
+
+#include<stdio.h>
+
+
+int main()
+{
+    char Arr[20] = {'\0'};
+
+    printf("Enter String : \n");
+    scanf("%[^'\n']s",Arr);
+   
+    printf("%s",Arr);
+
+    return 0;
+}
