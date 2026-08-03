37ce53c4131c1be135cd98f0b8c85035167a8b58
vedantmarne3 <vedantbmarne@gmail.com>
2026-08-03 23:53:06 +0530

    Added new peograms

---
+++ Competative/Problems_On_Pattern/program04.c
@@ -0,0 +1,29 @@
+/*
+    Input : 4
+    Output : #  1   *   #   2   *   #   3   *   #   4   *
+*/
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+    int iCnt = 0;
+    
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        
+        printf("#\t%d\t*\t",iCnt);
+        
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter no. of Element : \n");
+    scanf("%d",&iValue);
+
+    Pattern(iValue);
+
+    return 0;
+}
