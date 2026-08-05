8a7d46d33537ad771e1cc533cce7aa931ce169c4
vedantmarne3 <vedantbmarne@gmail.com>
2026-08-05 23:50:19 +0530

    Added new peograms

---
+++ Competative/Problems_On_Pattern/program13.c
@@ -0,0 +1,45 @@
+/*
+    Input : iRow = 3,  iCol = 4
+
+    Output : A  A   A   A   A
+             B  B   B   B   B
+             A  A   A   A   A
+
+*/
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+    char ch = '\0';
+
+
+    for(i = 1, ch = 'A'; i <= iRow; i++, ch++)
+    {
+        for(j = 1; j <= iCol;j++)
+        {
+            printf("%c\t",ch);
+        }
+        printf("\n");
+    }
+
+
+}
+
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter Number of Rows : \n");
+    scanf("%d",&iValue1);
+
+    printf("Enter Number of Columns : \n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1, iValue2);
+
+
+    return 0;
+}
