344a9af683e0ffa38e55a59ccedebb26bc1dbcee
vedantmarne3 <vedantbmarne@gmail.com>
2026-08-08 23:53:08 +0530

    New Program Added

---
+++ Competative/Problems_On_Pattern/program24.c
@@ -0,0 +1,51 @@
+/*
+    input : iRow = 5   iCol = 5
+    
+    Output :    1   2   3   4   5
+               -1  -2  -3  -4  -5
+               -1  -2  -3  -4  -5
+               -1  -2  -3  -4  -5
+
+*/
+
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(i == 1 || i == iRow)
+            {
+                printf(" %d\t",j);
+            }
+            else
+            {
+                printf("%d\t",-j);
+            }
+        }
+        printf("\n");
+    }
+
+
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter no. of Rows : \n");
+    scanf("%d",&iValue1);
+
+    printf("Enter no. of Colmns : \n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
