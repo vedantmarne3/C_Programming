b1bc89ee1c16c2e281acfdb4112e85e083d7155e
vedantmarne3 <vedantbmarne@gmail.com>
2026-08-22 23:56:29 +0530

    Added new programs

---
+++ Competative/Problems_On_Bit/program03.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+typedef unsigned int UINT;
+
+bool ChkBit(int iNo)
+{
+    UINT iMask = 0x8104040; // 7 15 21 28
+    UINT iAns = 0;
+
+    iAns = iMask & iNo;
+
+    if(iAns == iMask)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+    UINT iValue = 0;
+    bool bRet = 0;
+
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
+
+    bRet = ChkBit(iValue);
+
+    if(bRet == true)
+    {
+        printf("Bit is ON\n");
+    }
+    else
+    {
+        printf("Bit is OFF\n");
+    }
+
+    return 0;
+}
