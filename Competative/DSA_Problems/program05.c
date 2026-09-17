689feae41041f7b196881114dd62923449604e3a
vedantmarne3 <vedantbmarne@gmail.com>
2026-09-17 15:13:59 +0530

    Added new program

---
+++ Competative/DSA_Problems/program05.c
@@ -0,0 +1,85 @@
+//Counting Odd Nodes Value
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertLast(PPNODE first, int iNo)
+{
+    PNODE temp = NULL;
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = iNo;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        temp = *first;
+
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    while (first != NULL)
+    {
+        printf("| %d | -> ",first->data);
+        first = first->next;
+    }
+    printf("NULL\n");
+}
+
+void DisplyEven(PNODE first)
+{
+    
+
+    while (first != NULL)
+    {
+        if(first->data % 2 == 0)
+        {
+            printf("| %d | -> ",first->data);
+        }
+        first = first->next;
+    }
+    printf("NULL\n");
+   
+}
+
+int main()
+{
+    PNODE head = NULL;
+
+    InsertLast(&head,11);
+    InsertLast(&head,21);
+    InsertLast(&head,50);
+    InsertLast(&head,101);
+    InsertLast(&head,100);
+    InsertLast(&head,121);
+    InsertLast(&head,130);
+
+    Display(head);
+
+    DisplyEven(head);
+
+    return 0;
+}
